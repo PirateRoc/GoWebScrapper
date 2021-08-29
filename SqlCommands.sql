ef21a8de76a56cca65f7e3879f0c3042ba16fe12
PirateRoc <delfin.roc.bielsa@gmail.com>
2021-08-29 22:51:46 +0200

    Falla la conexion a DB desde Go Falta revisar si es culpa de la red o  de la configuracion

---
+++ SqlCommands.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE Ayuntamientos;
+USE Ayuntamientos;
+CREATE TABLE AyuntamientosAragon (
+    Poblacion varchar(100),
+    Email varchar(100),
+    Telefono varchar(9),
+    Web varchar(50) 
+);
