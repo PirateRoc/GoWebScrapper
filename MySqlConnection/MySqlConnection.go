fedd3e7917a1ccaeb80c49cd5f9e75766de789b2
PirateRoc <delfin.roc.bielsa@gmail.com>
2021-08-29 20:39:20 +0200

    Creada Conexion a Db  falta probar docker compose pero tiene buena pinta

---
+++ MySqlConnection/MySqlConnection.go
@@ -0,0 +1,31 @@
+package MySqlConnection
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/PirateRoc/GoWebScrapper/Ayuntamiento"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Insert(ayuntamientos []Ayuntamiento.Ayuntamiento) {
+
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/Ayuntamientos")
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmtIns, err := db.Prepare("INSERT INTO AyuntamientosAragon(Poblacion, Email,Telefono, Web) VALUES( ?, ?, ?, ? )")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	for _, ayuntamiento := range ayuntamientos {
+		_, err = stmtIns.Exec(ayuntamiento.Poblacion, ayuntamiento.Email, ayuntamiento.Telefono, ayuntamiento.Web) // Insert tuples (i, i^2)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+	}
+
+}
